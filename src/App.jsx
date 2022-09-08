0d35f90c8aee5b3cd5f8596092f9507befe2cbba
Daniel Silva <djs@cin.ufpe.br>
2022-09-08 19:35:31 -0300

    update App file with new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
-import { Header, Post } from "./components"
+import { Header, Post, Wrapper } from "./components"
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header/>
-      <Post
-        author='Daniel Silva'
-        content='Drawnings and movies'
-      />
-
-      <Post
-        author='Caio Barreto'
-        content='Music and entrepreneurship'
-      />
+      <Wrapper />
       <GlobalStyle/>
     </ThemeProvider>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import { Header } from "./Header"
 import { Post } from "./Post"
+import { Sidebar } from "./Sidebar"
+import { Wrapper } from "./Wrapper"
 
 export {
   Header,
-  Post
+  Post,
+  Sidebar,
+  Wrapper
 }
